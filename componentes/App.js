6352532d63684c611c890365ea19c203acab7eb4
Salustiano Oliveira <saluorodrigues@gmail.com>
2025-09-03 18:40:51 -0300

    Projeto Componentes

--- componentes/App.js
+++ componentes/App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//Import das bibliotecas
+import {View, Text, StyleSheet, Image, Button} from 'react-native';
 
-export default function App() {
-  return (
+
+//Função principal
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Primeira aula de React native!</Text>
+
+      <Image 
+        style={styles.imagem} 
+        source={{uri : 'https://static.vecteezy.com/ti/fotos-gratis/t2/42730459-leao-perigo-animal-animais-selvagens-natureza-floresta-foto.jpg'}}/>
+
+      <Image 
+        style={styles.imagem}
+        source={require('./assets/passaro.jpg')}/>
+
+        <Button 
+          title='Clique aqui' 
+          color={'orange'}
+          onPress={() => alert('React native!')}/>
     </View>
-  );
+  )
 }
 
+//Area de estilização
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    backgroundColor: '#ccc',
+    alignItems: 'center'
+  }, texto: {
+    fontSize: 24,
+    color: 'red',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+  }, imagem: {
+    width: 200,
+    height: 200,
+    marginBottom: 10,
+  }
+})
